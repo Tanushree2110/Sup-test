9393ee58e31bcdac71ff20848d5c88060f5c8a7d
Tanushree <54908596+Tanushree2110@users.noreply.github.com>
2020-10-07 15:45:26 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Quiz</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+
+		<!-- Navigation bar -->
+		<nav class="navbar navbar-expand-md justify-content-between bg-dark navbar-dark sticky-top">
+			<a class="navbar-brand" style="margin-left: 10%;" href="#">
+				<h3><b>Quiz Club</b></h3>
+			</a>
+			<ul class="navbar-nav" style="margin-right: 20%;">
+				<li class="nav-item" style="margin-right: 10%;">
+					<a class="nav-link active" href="#">
+						<h5>Home</h5>
+					</a>
+				</li>
+				<li class="nav-item" style="margin-right: 10%;">
+					<a class="nav-link" href="#about">
+						<h5>About</h5>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- Navigation bar -->
+		<div class="wave" style="margin-top: 0%;">
